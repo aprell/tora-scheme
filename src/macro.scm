1489fc93e0113ca9e8f59b9a8cfa0b6490a22935
Andreas Prell <andreas.h.prell@gmail.com>
2022-12-18 10:42:59 +0100

    Make local variable names in macros "unique"

--- src/macro.scm
+++ src/macro.scm
@@ -14,26 +14,26 @@
 
 ;; Named let defined in terms of letrec and lambda
 (define-macro (let= name bindings body)
-  (letrec ((cadr (lambda (lst) (car (cdr lst))))
-           (map (lambda (fn lst)
+  (letrec (($cadr (lambda (lst) (car (cdr lst))))
+           ($map (lambda (fn lst)
                   (if (null? lst) '()
-                      (cons (fn (car lst)) (map fn (cdr lst)))))))
+                      (cons (fn (car lst)) ($map fn (cdr lst)))))))
     `(letrec ((,name (lambda ,(map car bindings) ,body)))
-       (,name ,@(map cadr bindings)))))
+       (,name ,@($map $cadr bindings)))))
 
 (define-macro (switch expr cases)
-  (letrec ((test-equal (lambda (e pair)
+  (letrec (($test-equal (lambda (e pair)
                          (if (not (equal? `,(car pair) 'default))
                              `((equal? ,e ,(car pair)) ,@(cdr pair))
                              `(else ,@(cdr pair)))))
-           (map (lambda (fn lst)
+           ($map (lambda (fn lst)
                   (if (null? lst) '()
-                      (cons (fn (car lst)) (map fn (cdr lst)))))))
-  `(let ((t ,expr))
-     (cond ,@(map (lambda (pair) (test-equal 't pair)) cases)))))
+                      (cons (fn (car lst)) ($map fn (cdr lst)))))))
+  `(let (($t ,expr))
+     (cond ,@($map (lambda (pair) ($test-equal '$t pair)) cases)))))
 
 (define-macro (while test body)
-  `(let= loop () (when ,test (begin ,body (loop)))))
+  `(let= $loop () (when ,test (begin ,body ($loop)))))
 
 (define-macro (for i lo hi body)
   `(let ((,i ,lo))
@@ -44,10 +44,10 @@
 
 (define-macro (assert-equal a b)
   `(if (not (equal? ,a ,b))
-    (let ((msg (string-append
+    (let (($msg (string-append
                  "Assertion failed: "
                  ,(show a) " != " ,(show b) " <=> " ,a " != " ,b)))
-      (error msg))))
+      (error $msg))))
 
 (define-macro (assert-true a)
   `(assert-equal ,a #t))
